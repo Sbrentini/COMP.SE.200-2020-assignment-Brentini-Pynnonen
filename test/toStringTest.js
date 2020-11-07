2b9b5c0c3c197242ea467f130d0bb68a3d905833
Sara Brentini <brentini@student.tut.fi>
2020-11-07 11:58:35 +0200

    Fixed the hierachy of the project and created a unit test for toString.js

---
+++ test/toStringTest.js
@@ -0,0 +1,25 @@
+import toString from '../javascript-library/src/toString.js'
+import pkg from 'chai'
+const { assert } = pkg
+
+describe('Field test for toString', function (){
+
+
+    let str = 'this is a string';
+    let num = 5;
+    let array = ['This', 'is', 'an', 'Array', 6, 2, 6.00];
+
+    it('String to String', function(){
+        // toString shouldn't change the str values, and instead return as it is. 
+        assert.equal(toString(str), str);
+    })
+
+    it('Number to String', function(){
+        assert.isString(toString(num));
+    })
+
+    it('Array to String', function(){
+        assert.isString(toString(array));
+    })
+
+});
