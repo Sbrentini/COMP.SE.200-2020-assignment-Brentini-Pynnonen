df27020c5887e82a5d3be64874992b35485bc11d
Sannamari Pynnönen <sannamari.pynnonen@icloud.com>
2020-11-10 11:24:06 +0200

    Added unit tests for ceil.js

---
+++ test/ceilTest.js
@@ -0,0 +1,15 @@
+import ceil from '../javascript-library/src/ceil.js'
+import pkg from 'chai'
+const { assert } = pkg
+
+describe('Field test for ceil', function(){
+
+    it('Round up to integer', function (){
+        assert.equal(ceil(3.004), 4)
+    });
+
+    it('Round up to two decimals', function(){
+        assert.equal(ceil(3.004, 2), 3.01)
+    })
+
+})
