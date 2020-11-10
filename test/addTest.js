08e3f3757940b6119b1787c04d4b6be574e8112d
Sannamari Pynnönen <sannamari.pynnonen@icloud.com>
2020-11-10 11:02:37 +0200

    Added unit tests for add.js

--- test/addTest.js
+++ test/addTest.js
@@ -4,8 +4,12 @@ const { assert } = pkg
 
 describe('Field test for add', function(){
 
-    it('Test 1', function (){
+    it('Add positive numbers', function (){
         assert.equal(add(5, 7), 12)
+    });
+
+    it('Add positive and negative numbers', function(){
+        assert.equal(add(5, -5), 0)
     });
     
 })
