9e5cba35a224798719839bcade9144c03092a674
Sannamari Pynnönen <sannamari.pynnonen@icloud.com>
2020-11-11 11:33:07 +0200

    Added unit tests for reduce.js

---
+++ test/reduceTest.js
@@ -0,0 +1,11 @@
+import reduce from '../javascript-library/src/reduce.js'
+import pkg from 'chai'
+const { assert } = pkg
+
+describe('Field test for reduce', function(){
+
+    it('Test 1', function (){
+        assert.equal(reduce([1, 2], (sum, n) => sum + n), 3)
+    });
+    
+})
