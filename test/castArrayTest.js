20089a7a9d965133ced9b8e7eb8007c426051e2c
Sara Brentini <sara.brentini@tuni.fi>
2020-11-24 16:00:49 +0200

    unit tests for castArray.js and countBy.js. Initialised at.js, get.js, and map.js test files

---
+++ test/castArrayTest.js
@@ -0,0 +1,47 @@
+import castArray from '../javascript-library/src/castArray.js'
+import pkg from 'chai'
+const { assert } = pkg
+
+describe('Field test for cast array', function(){
+
+    let str = 'test';
+    let num = 11;
+    let float = 2.4;
+    let strSentence = 'This is a sentence, what happens now?';
+    let array1 = ['one', 'two', 'three', '', 0]
+    let empty1 = '';
+    let empty2 = null;
+    let object = {a:'test1', b:200, c:'test2'};
+
+    it('Testing string cast to Array', function(){
+        assert.isArray(castArray(str));
+    })
+
+    it('Testing number cast to Array', function(){
+        assert.isArray(castArray(num));
+    })
+
+    it('Testing float cast to Array', function(){
+        assert.isArray(castArray(float));
+    })
+
+    it('Testing string sentence cast to Array', function(){
+        assert.isArray(castArray(strSentence));
+    })
+
+    it('Testing array cast to Array', function(){
+        assert.equal(castArray(array1), array1);
+    })
+
+    it('Testing empty cast to Array', function(){
+        assert.isArray(castArray(empty1));
+    })
+
+    it('Testing null cast to Array', function(){
+        assert.isArray(castArray(empty2));
+    })
+
+    it('Testing object cast to Array', function(){
+        assert.isArray(castArray(object));
+    })
+})
---
+++ test/countByTest.js
@@ -0,0 +1,29 @@
+import countBy from '../javascript-library/src/countBy.js'
+import pkg from 'chai'
+const { assert } = pkg
+
+
+describe('Field test for counting occurances from a collection', function (){
+    
+    const products = [
+        {'name': 'apple', 'cost': 1.00, 'type': 'fruit', 'available': true},
+        {'name': 'banana', 'cost': 1.50, 'type': 'fruit', 'available': true},
+        {'name': 'orange', 'cost': 2.00, 'type': 'fruit', 'available': true},
+        {'name': 'cucubmer', 'cost': 2.50, 'type': 'vegetable', 'available': true},
+        {'name': 'chicken', 'cost': 9.00, 'type': 'meat', 'available': false},
+        {'name': 'pork', 'cost': 7.00, 'type': 'meat', 'available': true},
+        {'name': 'spinach', 'cost': 4.00, 'type': 'vegetable', 'available': false},
+        {'name': 'fish', 'cost': 12.00, 'type': 'seafood', 'available': false}
+    ];
+
+    const typeObject = { fruit: 3, vegetable: 2, meat: 2, seafood: 1 };
+    const availableObject = { true: 5, false: 3 };
+
+    it('Counting the number of types from an imaginery product object array', function(){
+        assert.equal(countBy(products, value => value.type), typeObject);
+    })
+
+    it('Counting the number of available products from an imaginery product object array', function(){
+        assert.equal(countBy(products, value => value.available), availableObject);
+    })
+})
