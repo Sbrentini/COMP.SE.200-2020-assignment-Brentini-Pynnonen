47a5099f67eb914f9a302df8eda65cde16a7e7c0
Sara Brentini <sara.brentini@tuni.fi>
2020-11-26 11:52:33 +0200

    updated readme and added a test for at.js

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # COMP.SE.200-2020-assignment-Brentini-Pynnonen
+[![Build Status](https://travis-ci.org/Sbrentini/COMP.SE.200-2020-assignment-Brentini-Pynnonen.svg?branch=main)](https://travis-ci.org/Sbrentini/COMP.SE.200-2020-assignment-Brentini-Pynnonen)
+
 [![Coverage Status](https://coveralls.io/repos/github/Sbrentini/COMP.SE.200-2020-assignment-Brentini-Pynnonen/badge.svg?branch=main)](https://coveralls.io/github/Sbrentini/COMP.SE.200-2020-assignment-Brentini-Pynnonen?branch=main)
 
-Tämä on testi
+Project work part 2 for the course COMP.SE.200-2020-2021 Software testing. The purpose of this project is to test a given javascript library (https://github.com/tgcslearningtech/COMP.SE.200-2020-assignment) and create tests according to a test plan that was created during part 1. 
 
-Tämä on testi2
+This project uses packages such as: Mocha, Chai, C8 and mocha-lcov-reporter to aid with testing and to create a coverage report. The project is deployed to Travis CI and coverage data is send to Coveralls. 
+
+The project was tested with Node v.14.15.0 and npm v.6.14.8. To install the project run: 
+- npm install -g npm@latest
+- npm install --dev
---
+++ test/atTest.js
@@ -0,0 +1,35 @@
+import at from '../src/at.js'
+import pkg from 'chai'
+const { assert } = pkg
+
+describe('Field test for at', function() {
+
+    const product = {'name': 'apple', 'cost': 1.00, 'type': null, 'available': true};
+
+    const producer = {
+        'name': 'Fresh Apples', 
+        'products': { 
+            'allProducts': ['normal apple', 'green apple', 'red apple'] } };
+    
+    const expectedProduct = [ 'apple' ]
+    const expectedProductList = [ 'normal apple', 'green apple', 'red apple' ]
+
+    it('Find the name of a product with at', function (){
+        assert.deepEqual(at(product, 'name'), expectedProduct);
+    });
+
+    it('Testing null value with at', function(){
+        assert.isOk(at(product, 'name'));
+    });
+
+    it('Find list in a nested object with at', function() {
+        assert.deepEqual(at(producer, ['products.allProducts[0]', 'products.allProducts[1]',
+         'products.allProducts[2]']), expectedProductList);
+    })
+
+    // This really depends if it should be okay or not, since an unbound list should be thrown as an error
+    it('Find list in a nested object with at - added an unbound list value', function() {
+        assert.isOk(at(producer, ['products.allProducts[0]', 'products.allProducts[1]',
+         'products.allProducts[2]', 'products.allProducts[3]']));
+    })
+})
